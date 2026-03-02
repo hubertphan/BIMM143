cd35cdc2dccd66eb1b12feda9cbb0c218804b86e
hubertphan <hubertphan04@gmail.com>
2026-03-02 01:26:44 -0800

    add class9, lab worksheets, class04-07

---
+++ class04/class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Hubert Phan"
+#' date: "January 15, 2026"
+#' ---
+#' 
+x <- 1:50
+plot(x, sin(x))
+
+plot(x, sin(x), type = "l", col = "blue", lwd = 2,
+     main = "Sine Function", xlab = "X values", ylab = "Sine of X")
+
+log(10, base = 10)
